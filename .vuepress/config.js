be6e272ea5341d794dc8bf800f5409b82cfac64e
Signaturr <mysignaturr@gmail.com>
2018-10-29 20:00:55 -0400

    Initial commit

---
+++ .vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'Mr. M. VuePress!',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Blog', link: '/blog/' },
+            { text: 'Hello', link: '/hello.md' }
+        ],
+        sidebar: [
+            '/',
+            '/blog/'
+        ]
+    }
+}
